352a14a72de298e90dc681ced688a8fc32221227
Brayden Cleary <brayden.cleary@gmail.com>
2014-02-14 08:56:43 -0800

    Sketching out how I want to store "adjacent mine" data

--- minesweeper.js
+++ minesweeper.js
@@ -2,6 +2,11 @@ var app = app || {};
 
 app.Board = Backbone.View.extend({
   el: '.board',
+  indicesOfMines: [],
+
+  initialize: function(){
+    //call function
+  },
 
   render: function(){
     this.collection.each(function(tile){
@@ -14,8 +19,38 @@ app.Board = Backbone.View.extend({
       model: tile
     });
     this.$el.append(tileView.render().el)
+  },
+
+  gatherIndicesOfMines: function(){
+    this.collection.each(function(tile, index){
+      if (tile.get("type") == "mine") {
+        this.indicesOfMines.push(index)
+      }
+    }, this);
+    return this.indicesOfMines
+  },
+
+  setSomething: function(){
+    this.collection.each(function(tile, index){
+
+    })
   }
 })
+
+//hardcoding in 8x8
+//Indicies I care about for each tile 1 +8 -1 +9 +7 -9 -8 -7
+
+/* var neighboringInfo = Backbone.Model({
+    topLeft: 'mine',
+    top: 'land',
+    topRight: 'land',
+    left: 'land',
+    right: 'land'
+    bottomLeft: 'mine',
+    bottom: 'land',
+    bottomRight: 'land'
+// });
+*/
 
 app.TileView = Backbone.View.extend({
   tagName: 'div',
@@ -59,7 +94,7 @@ $(function(){
     app.tilesCollection.add(new app.Tile({type: "mine"}))
   })
 
-  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle())})
+  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: 8})
   board.render();
 })
 
