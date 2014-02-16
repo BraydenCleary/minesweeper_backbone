ff2adbd7adf3dc667e670a9dea8a9aa127d9b81f
Brayden Cleary <brayden.cleary@gmail.com>
2014-02-15 22:11:10 -0800

    Making mine count dynamic...need to add validations for mine and board size

--- minesweeper.js
+++ minesweeper.js
@@ -1,42 +1,38 @@
 var app = app || {};
 
+app.resetGame = function(){
+  $('.board').empty();
+  app.tilesCollection.reset()
+
+  var rowLength = parseInt($('#row-length').val());
+  var mineCount = parseInt($('#mine-count').val())
+  var totalTiles = rowLength * rowLength
+  var landCount = totalTiles - mineCount
+
+  _.times(landCount, function(){
+    app.tilesCollection.add(new app.Tile({type: "land"}));
+  })
+
+  _.times(mineCount, function(){
+    app.tilesCollection.add(new app.Tile({type: "mine"}));
+  })
+
+  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: rowLength});
+  board.render();
+  $('.tile').css('width', 100/rowLength + "%")
+}
+
+
 $(function(){
   app.vent = _.extend({}, Backbone.Events);
 
   app.tilesCollection = new Backbone.Collection();
 
-  _.times(54, function(){
-    app.tilesCollection.add(new app.Tile({type: "land"}));
+  $('#row-length, #mine-count').on('change', function(){
+    app.resetGame()
   })
 
-
-  _.times(10, function(){
-    app.tilesCollection.add(new app.Tile({type: "mine"}));
-  })
-
-  $('#row-length').on('change', function(){
-    $('.board').empty();
-    app.tilesCollection.reset()
-
-    var rowLength = parseInt($('#row-length').val());
-    var totalTiles = rowLength * rowLength
-    var landCount = totalTiles - 10
-
-    _.times(landCount, function(){
-      app.tilesCollection.add(new app.Tile({type: "land"}));
-    })
-
-    _.times(10, function(){
-      app.tilesCollection.add(new app.Tile({type: "mine"}));
-    })
-
-    board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: rowLength});
-    board.render();
-    $('.tile').css('width', 100/rowLength + "%")
-  })
-
-  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: parseInt($('#row-length').val())});
-  board.render();
+  app.resetGame();
 })
 
 
