faf847a08cfca31b929b7bc94ea8536bb5c92b8d
Brayden Cleary <brayden.cleary@gmail.com>
2014-02-15 21:52:34 -0800

    Making board size dynamic

--- minesweeper.js
+++ minesweeper.js
@@ -14,7 +14,28 @@ $(function(){
     app.tilesCollection.add(new app.Tile({type: "mine"}));
   })
 
-  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: 8});
+  $('#row-length').on('change', function(){
+    $('.board').empty();
+    app.tilesCollection.reset()
+
+    var rowLength = parseInt($('#row-length').val());
+    var totalTiles = rowLength * rowLength
+    var landCount = totalTiles - 10
+
+    _.times(landCount, function(){
+      app.tilesCollection.add(new app.Tile({type: "land"}));
+    })
+
+    _.times(10, function(){
+      app.tilesCollection.add(new app.Tile({type: "mine"}));
+    })
+
+    board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: rowLength});
+    board.render();
+    $('.tile').css('width', 100/rowLength + "%")
+  })
+
+  board = new app.Board({collection: app.tilesCollection.reset(app.tilesCollection.shuffle()), rowLength: parseInt($('#row-length').val())});
   board.render();
 })
 
